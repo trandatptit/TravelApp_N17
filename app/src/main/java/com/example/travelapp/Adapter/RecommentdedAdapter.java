b96b2fec2d7013065759f2ed976dda819a7a2f9a
nam <namtran10a2nt@gmail.com>
2025-04-10 10:20:24 +0700

    chuc nang tourDetail

--- app/src/main/java/com/example/travelapp/Adapter/RecommentdedAdapter.java
+++ app/src/main/java/com/example/travelapp/Adapter/RecommentdedAdapter.java
@@ -10,7 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-//import com.example.travelapp.activity.Activity.DetailActivity;
+import com.example.travelapp.Activity.DetailActivity;
 import com.example.travelapp.Domain.ItemDomain;
 import com.example.travelapp.databinding.ViewholderRecommendedBinding;
 
@@ -44,15 +44,15 @@ public class RecommentdedAdapter extends RecyclerView.Adapter<RecommentdedAdapte
                 .load(items.get(position).getPic())
                 .into(binding.pic);
 
-//        holder.itemView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(context, DetailActivity.class);
-//                intent.putExtra("object", items.get(position));
-//                context.startActivity(intent);
-//
-//            }
-//        });
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, DetailActivity.class);
+                intent.putExtra("object", items.get(position));
+                context.startActivity(intent);
+
+            }
+        });
     }
 
     @Override
